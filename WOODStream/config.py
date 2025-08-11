4194c077289f46e31d281de7a1cecf3f8c3e8205
seeubot <155578467+seeubot@users.noreply.github.com>
2025-08-11 15:58:41 +0530

    Update config.py

--- WOODStream/config.py
+++ WOODStream/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Telegram:
-    API_ID = int(env.get("API_ID",23054736))
+    API_ID = int(env.get("API_ID", '23054736'))
     API_HASH = str(env.get("API_HASH"))
     BOT_TOKEN = str(env.get("BOT_TOKEN"))
     OWNER_ID = int(env.get('OWNER_ID', '7978482443'))
